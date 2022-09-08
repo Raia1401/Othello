524f9e31baf6f6406d70dbb894d379134b9ec560
sakurai <ryou3276kysaku@gmail.com>
2022-09-08 19:14:00 +0900

    mainファイルを作成

--- backend/main.go
+++ backend/main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
     router := gin.Default()
-    router.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+
+    othelloRouter := router.Group("/othello")
+    {
+        v1 := othelloRouter.Group("/v1")
+        // v1.GET("/get-stone")
+        v1.POST("/make-board")
+        v1.PUT("/update-stone")
+    }
+
     router.Run(":8080")
 }
