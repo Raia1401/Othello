de0f48ae9884e0c9cb2aa2d338301907aa19f8dc
sakurai <ryou3276kysaku@gmail.com>
2023-05-21 11:11:27 +0900

    不要な処理を削除

--- backend/service/test.go
+++ backend/service/test.go
@@ -9,27 +9,9 @@ import (
 
 type TestService struct{}
 
-// func (TestService) GetUserTest() bool {
-// 	user := model.User{UserId: 1}
-// 	fmt.Println(engine)
-// 	// result, err := engine.Where("user_id = ?", 1).Get(&user)
-// 	result, err := engine.Get(&user)
-// 	// fmt.Println(err)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	if !result {
-// 		log.Fatal("Not Found")
-// 	}
-// 	fmt.Println("user:", result)
-// 	return result
-// }
-
 func (TestService) BoardTest() {
 	board := board.NewBoard()
 	fmt.Println(board.StonesPos)
-	//fmt.Println(board.PlaceabilityPos)
-	//fmt.Println(board.PlaceabilityDir)
 
 	//石を(3,4)に置く
 	ok, err := board.PutDownStone(3, 4, 1)
