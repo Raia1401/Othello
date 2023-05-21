04d2babc6f55dfe16faca8448954eb44b5f54419
sakurai <ryou3276kysaku@gmail.com>
2023-05-21 10:47:01 +0900

    使っていないコードを削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,14 @@ import './App.css';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 
 import { BoardDataProvider } from './providers/boardDataProvider';
+
 import GamePage from './pages/gamePage';
+// import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/' element={
-          <BoardDataProvider>
-            <LoginPage />
-          </BoardDataProvider>
-        } /> */}
-
         <Route path='/' element={
                 <BoardDataProvider>
                   <GamePage/>
--- frontend/src/hooks/useBoardData.tsx
+++ frontend/src/hooks/useBoardData.tsx
@@ -21,27 +21,15 @@ const convStringToArray = (rawStonesPos:string) => {
   return col
 }
 
-
 export const useBoardData=(boardId:number,isMyTurn:boolean)=>{
 
-    // const userDataCtx=useContext(UserDataContext)
-    // const [boardId,setBoardId]=useState<number>(0)
-    // const [isMyTurn,setIsMyturn]=useState<boolean>(false)
     const [stonesPos,setStonesPos]=useState<number[][]>([])
     const [isMatchEnd,setIsMatchEnd]=useState<boolean>(false)
 
-    // const {boardId,setBoardId} = useContext(BoardDataContext)
-    // userDataCtx.userId
-
     useEffect(()=>{
       apis.getBoardData(boardId).then((boardData:BoardData)=>{
-        // console.log("boardData.data.Board",boardData.data.Board)
-        // const stonePosArray = convStringToArray(boardData.data.Board)
-        // console.log("stonePosArray",stonePosArray)
-        // setBoardId(boardData.data.BoardId)
         setStonesPos(convStringToArray(boardData.data.Board))
         setIsMatchEnd(boardData.data.IsMatchEnd)
-        // setIsMyturn(boardData.data.IsMyTurn)
       })
     },[boardId,isMyTurn])
 
